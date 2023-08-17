2bb68a7e9c68ef3daff21625671429206398a366
bavarianprayoga <bavrianz@gmail.com>
2023-08-17 23:20:59 +0700

    w

---
+++ arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int x = 5;
+int y = 2;
+
+// float z = x / (float) y;
+
+// //*(float) di depan "y" buat convert data typenya
+
+// printf("%.2f", z);
+
+int z = x % y;
+
+printf("%d", z);
+
+    return 0;
+}
---
+++ testingvariable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    float apel = 5.75;
+    float kolor = 99.99;
+    float akua = 1.99;
+
+    printf("Kolor: $%.2f\n", kolor);
+    printf("Apel: $%.2f\n", apel);
+    printf("Akua: $%.2f\n", akua);
+
+    return 0;
+}
