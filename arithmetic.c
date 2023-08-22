fc3fd0010f2e6686b4b5c5592a915134b666e7eb
bavarianprayoga <bavrianz@gmail.com>
2023-08-22 17:37:08 +0700

    ncie

--- arithmetic.c
+++ arithmetic.c
@@ -3,17 +3,36 @@
 int main(){
 
 int x = 5;
-int y = 2;
+// int y = 2;
 
-// float z = x / (float) y;
+// // float z = x / (float) y;
 
-// //*(float) di depan "y" buat convert data typenya
+// // //*(float) di depan "y" buat convert data typenya
 
-// printf("%.2f", z);
+// // printf("%.2f", z);
 
-int z = x % y;
+// int z = x % y;
 
-printf("%d", z);
+// printf("%d", z);
+
+//!
+
+// x = x + 2;
+// x+=2;
+
+// x = x - 3;
+// x-=3;
+
+// x = x * 2;
+// x*=2;
+
+// x = x / 5;
+// x/=5;
+
+// x = x % 2;
+// x%=2;
+
+//* augmented assignment operators = shortcut basically
 
     return 0;
 }
