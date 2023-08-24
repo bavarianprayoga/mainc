fbbf30bf041b32a662100993e70c66217eb644f1
bavarianprayoga <bavrianz@gmail.com>
2023-08-24 19:18:41 +0700

    yea

--- ifsandswitch.c
+++ ifsandswitch.c
@@ -2,23 +2,51 @@
 
 int main(){
 
-    int age;
+    // int age;
 
-    printf("Enter your age: ");
-    scanf("%d", &age);
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
 
-    if(age >= 18, age < 80){
-        printf("congrats, you can suffer alone.");
+    // if(age >= 18, age < 80){
+    //     printf("congrats, you can suffer alone.");
+    // }
+    // else if(age >= 80){
+    //     printf("better write your will");
+    // }
+    // else if(age < 0){
+    //     printf("dumbass");
+    // }
+    // else{
+    //     printf("get outta here");
+    // }
+
+    //* daripada pake banyak else if, mending pake switch, Tapi, switch cuma bisa exact comparison.
+
+    int grade;
+
+    printf("Enter your letter grade: ");
+    scanf("%s", &grade);
+    
+    switch(grade){
+        case 'A':
+            printf("weelll");
+            break;
+        case 'B':
+            printf("mantep");
+            break;
+        case 'C':
+            printf("mayan");
+            break;
+        case 'D':
+            printf("yauda la ya");
+            break;
+        case 'F':
+            printf("goblok");
+            break;
+        default:
+            printf("yang bener lah anjg");
     }
-    else if(age >= 80){
-        printf("better write your will");
-    }
-    else if(age < 0){
-        printf("dumbass");
-    }
-    else{
-        printf("get outta here");
-    }
+
 
     return 0;
 }
