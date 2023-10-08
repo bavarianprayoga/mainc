b104ecb7e3de3f27ea9c4515c21055192474dceb
bavarianprayoga <bavrianz@gmail.com>
2023-10-08 20:38:17 +0700

    YEA

--- luwakwhitekopi.c
+++ luwakwhitekopi.c
@@ -1,22 +1,20 @@
-#include <stdio.h>  
+#include <stdio.h>
+
+void mult(int a[4], int b[4]){
+    for(int i = 0; i < 4; i++){
+        printf("%d\n", a[i] * b[i]);
+    }
+}
 
 int main(){  
 
-    int a[4], b[4], c[4], d[4]; 
-    double sum[4];
+    int a[4], b[4];
 
-    for(int i = 0; i < 3; i++){
-        scanf("%d %d %d %d", &a[i], &b[i], &c[i], &d[i]);
-        a[i] *= 2;
-        b[i] *= 2;
-        c[i] *= 2;
-        sum[i] = (double)a[i] + b[i] + c[i] +d[i];
+    for(int i = 0; i < 4; i++){
+        scanf("%d %d", &a[i], &b[i]);
     }
 
-    for(int i = 0; i < 3; i++){
-        printf("%.2lf\n", sum[i]);
-    }
-
+    mult(a, b);
 
     return 0;
 }
